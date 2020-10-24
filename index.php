10bcde551433f7ebff4de844a1eab05b6cef4b43
luksk <lukskill@hotmail.com>
2020-10-24 15:13:10 -0300

    arquivo modificado

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>operadores logicos</title>
+</head>
+<body>
+    <?php
+        if(5 == 5 OR 10 === 3) {
+            echo 'verdadeiro';
+        } else {
+            echo 'falso';
+        }
+        
+        echo '<br/ >';
+        
+        if (10 !== 8 && 5 <> 3 && 5 != 5) {
+            echo 'vdd';
+        } else {
+            echo 'mintira';
+        }
+    ?>
+</body>
+</html>
+<
